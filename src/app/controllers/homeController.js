a16b478b08f36a53243458b9975a914b7f9beebf
Socical Story <cuonghamanhcuong12@gmail.com>
2022-06-10 01:17:16 +0700

    fist node

---
+++ src/app/controllers/homeController.js
@@ -0,0 +1,19 @@
+const {con, sql} = require('../../model/connect');
+const sanphamModel = require('../../model/sanpham.model');
+
+class homeController{
+  //get all data
+    index(req, res) {
+      sanphamModel.getSanPhamDanhMuc((err,danhmuc,sanpham)=>{
+        res.send({sanpham:sanpham,danhmuc: danhmuc });
+        // res.render('home', {
+        //   showFooter: true,
+        //   showHeader: false,
+        //   layout: 'layoutDefaut.hbs',
+        //   danhmuc: danhmuc,
+        //   sanpham: sanpham
+        // });
+      })
+    }
+}
+module.exports = new homeController
