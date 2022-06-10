b81368dfb3f040a26aecb58eb8b16aba98ac3d20
Socical Story <cuonghamanhcuong12@gmail.com>
2022-06-10 10:13:28 +0700

    config env

--- src/model/connect.js
+++ src/model/connect.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const config = {
-    user: 'sa',
-    password: '123456',
-    server: '127.0.0.1',
-    database: 'FuijtShop',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    dialect: 'postgres',
     synchronize: true,
     options: {
         encrypt: false,
