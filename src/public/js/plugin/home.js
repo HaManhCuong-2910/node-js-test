1025330d4bed67150f473c5aa5bf2c2a824e0b6c
Socical Story <cuonghamanhcuong12@gmail.com>
2022-10-06 11:17:14 +0700

    update 06-10-2022

--- src/public/js/plugin/home.js
+++ src/public/js/plugin/home.js
@@ -1,6 +1,47 @@
 var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".itravel-sildeHome-button-next",
-      prevEl: ".itravel-sildeHome-button-prev",
+  navigation: {
+    nextEl: ".itravel-sildeHome-button-next",
+    prevEl: ".itravel-sildeHome-button-prev",
+  },
+  on: {
+    slideChange: function () {
+      let index_currentSlide = swiper.realIndex;
+      let currentSlide = swiper.slides[index_currentSlide];
+      console.log(currentSlide);
     }
-  });
+  }
+});
+var swiperBanner = new Swiper(".swiperBanner", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true
+  }
+});
+
+$( ".home-news_contain_title_list__imgs div img" ).hover(
+  function() {
+    let srcPri = $(this).attr('data-src');
+    let arrSrc = srcPri.split('.');
+    let nameSrc = arrSrc[0];
+    let newSrc = $(this).attr('src');
+    if(!newSrc.includes('_primary')){
+      newSrc = newSrc.replace(nameSrc,nameSrc + '_primary')
+    }    
+
+    $(this).fadeOut(60,function(){
+      $(this).attr('src',newSrc);
+    }).fadeIn(60) 
+  }, function() {
+    $(this).fadeOut(60,function(){
+      $(this).attr('src', $(this).attr('src').replace('_primary','') );
+    }).fadeIn(60);
+  }
+);
+let homeMore = document.querySelector('#home-more');
+window.addEventListener('scroll',(event)=>{
+  if(homeMore.offsetTop - window.scrollY < window.innerHeight + 100){
+    console.log(homeMore.offsetTop - window.scrollY);
+    $('.home-more_vector').css('display','block');
+  }
+})
